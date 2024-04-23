be81f0cabca9245aa1931386d997e7e098c3edef
gox1997 <goran.skaljac97@gmail.com>
2024-04-23 12:31:44 +0200

    script problem

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f262b89876df652"; }
+/******/ 		__webpack_require__.h = function() { return "ec6e11e873d3a65d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
